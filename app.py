3ba2887c0ef618a4718ddd3904e5338e7caff70e
Subhabrata Nath <85347886+subha996@users.noreply.github.com>
2021-07-08 12:09:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+# importing my apps
+from multiapp import MultiApp
+from Classification import clf1
+from Regression import mlv2_V2
+from eda_pd_profiling import eda3
+import home, about
+
+# creating multi app class instance
+app = MultiApp()
+
+# adding all apps
+app.add_app('Home', home.app)
+app.add_app('Data Profiling', eda3.app)
+app.add_app('Regression', mlv2_V2.app)
+app.add_app('Classification', clf1.app)
+app.add_app('About & Detailed Project Report', about.app)
+
+# running the current app which will run other apps.
+app.run()
